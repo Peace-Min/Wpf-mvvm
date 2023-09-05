889fe2e204064336f48102ad72a4f5e42d942bbc
민평화 <20181740@o365.hanbat.ac.kr>
2023-09-05 15:16:20 +0900

    초기 커밋.

---
+++ WpfApp5/Model/Mainmodel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WpfApp5.Model
+{
+    internal class Mainmodel : INotifyPropertyChanged
+    {
+        private int num = 1;
+        private int num2 = 1;
+        public int Num
+        {
+            set
+            {
+                num = value;
+                OnPropertyChanged("Num");
+            }
+            get
+            {
+                return num;
+            }
+        }
+        public int Num2
+        {
+            get { return num2; }
+            set
+            {
+                num2 = value;
+                OnPropertyChanged("Num2");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (PropertyChanged != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
---
+++ WpfApp5/ViewModel/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp5.ViewModel
+{
+    internal class MainViewModel : INotifyPropertyChanged
+    {
+        private Model.Mainmodel model = null;
+        public Command btn_cmd { get; set; }
+        public MainViewModel() { 
+            model = new Model.Mainmodel();
+            btn_cmd = new Command(Execute_func, CanExecute_func);
+        }
+        public Model.Mainmodel Model
+        {
+            get { return model; }
+            set { model = value; OnPropertyChanged("Model");
+            }
+        }
+        private void Execute_func(object obj)
+        {
+            model.Num2 = Model.Num * 2;
+        }
+        private bool CanExecute_func(object obj)
+        {
+            return true;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (PropertyChanged != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
